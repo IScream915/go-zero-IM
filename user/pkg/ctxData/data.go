5ff0f8096ce5f6e4def728924d4bca0af6e7d45c
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-08-12 18:15:51 +0800

    新增jwt包

--- user/pkg/ctxData/data.go
+++ user/pkg/ctxData/data.go
@@ -1,17 +1,1 @@
 package ctxData
-
-import "github.com/golang-jwt/jwt/v4"
-
-const Identify = "userCtx"
-
-func GetJwtToken(secretKey string, iat, expiredTime int64, uid string) (string, error) {
-	claims := make(jwt.MapClaims)
-	claims["exp"] = expiredTime
-	claims["iat"] = iat
-	claims[Identify] = uid
-
-	token := jwt.New(jwt.SigningMethodHS256)
-	token.Claims = claims
-
-	return token.SignedString([]byte(secretKey))
-}
