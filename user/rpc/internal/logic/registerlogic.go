7dba4760705d773db7633ede10838692ae98a8eb
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-08-12 18:16:10 +0800

    实现用户注册方法

--- user/rpc/internal/logic/registerlogic.go
+++ user/rpc/internal/logic/registerlogic.go
@@ -2,11 +2,25 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"time"
+	"user/dao/models"
+	"user/dao/query/implement"
+	"user/pkg/ctxData"
+	"user/pkg/encrypt"
+	"user/pkg/wuid"
 
 	"user/rpc/internal/svc"
 	"user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+)
+
+var (
+	ErrPhoneIsRegistered = errors.New("该手机号已经注册过")
+	ErrEncrypt           = errors.New("密码加密错误")
+	ErrInsertUser        = errors.New("创建新用户失败")
+	ErrGenToken          = errors.New("生成token失败")
 )
 
 type RegisterLogic struct {
@@ -24,7 +38,46 @@ func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Register
 }
 
 func (l *RegisterLogic) Register(in *user.RegisterReq) (*user.RegisterResp, error) {
-	// todo: add your logic here and delete this line
+	// 查询手机号是否已经注册
+	dbTool := implement.NewDbToolHelper[models.User](l.svcCtx.DB)
+	record, err := dbTool.SearchSingleByField(l.ctx, "phone", in.Phone)
+	if err != nil {
+		return nil, errors.New("查询时出现错误")
+	}
 
-	return &user.RegisterResp{}, nil
+	// 如果查询的到手机号则说明已经注册过, 返回报错
+	if record != nil {
+		return nil, ErrPhoneIsRegistered
+	}
+
+	// 注册信息登记
+	// 对密码加密
+	genPassword, err := encrypt.GenPasswordHash([]byte(in.Password))
+	if err != nil {
+		return nil, ErrEncrypt
+	}
+
+	newUser, err := dbTool.InsertSingleRecordAndReturn(l.ctx, &models.User{
+		ID:       wuid.GenUid(svc.DSN),
+		Nickname: in.Nickname,
+		Phone:    in.Phone,
+		Password: string(genPassword),
+		Status:   0,
+		Sex:      int(in.Sex),
+	})
+	if err != nil {
+		return nil, ErrInsertUser
+	}
+
+	// jwt-token生成
+	nowTime := time.Now().Unix()
+	token, err := ctxData.GetJwtToken(l.svcCtx.Config.Jwt.AccessSecret, nowTime, l.svcCtx.Config.Jwt.AccessExpire, newUser.ID)
+	if err != nil {
+		return nil, ErrGenToken
+	}
+
+	return &user.RegisterResp{
+		Token:  token,
+		Expire: nowTime + l.svcCtx.Config.Jwt.AccessExpire,
+	}, nil
 }
