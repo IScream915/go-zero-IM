975374908c9ad0e5823e51030987a549bf586652
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-08-12 19:36:04 +0800

    新增用户登录功能

--- user/rpc/internal/logic/loginlogic.go
+++ user/rpc/internal/logic/loginlogic.go
@@ -2,11 +2,22 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"time"
+	"user/dao/models"
+	"user/dao/query/implement"
+	"user/pkg/ctxData"
+	"user/pkg/encrypt"
 
 	"user/rpc/internal/svc"
 	"user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+)
+
+var (
+	ErrPhoneIsNotRegistered = errors.New("该手机号未进行注册")
+	ErrPassword             = errors.New("密码错误")
 )
 
 type LoginLogic struct {
@@ -24,7 +35,33 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 }
 
 func (l *LoginLogic) Login(in *user.LoginReq) (*user.LoginResp, error) {
-	// todo: add your logic here and delete this line
+	// 根据手机号进行查询
+	dbTool := implement.NewDbToolHelper[models.User](l.svcCtx.DB)
+	record, err := dbTool.SearchSingleByField(l.ctx, "phone", in.Phone)
+	if err != nil {
+		return nil, errors.New("查询时出现错误")
+	}
 
-	return &user.LoginResp{}, nil
+	// 查询不到手机号, 直接返回
+	if record == nil {
+		return nil, ErrPhoneIsNotRegistered
+	}
+
+	// 校验密码
+	if !encrypt.ValidatePasswordHash(in.Password, record.Password) {
+		// 校验失败, 直接返回
+		return nil, ErrPassword
+	}
+
+	// jwt-token生成
+	nowTime := time.Now().Unix()
+	token, err := ctxData.GetJwtToken(l.svcCtx.Config.Jwt.AccessSecret, nowTime, l.svcCtx.Config.Jwt.AccessExpire, record.ID)
+	if err != nil {
+		return nil, ErrGenToken
+	}
+
+	return &user.LoginResp{
+		Token:  token,
+		Expire: nowTime + l.svcCtx.Config.Jwt.AccessExpire,
+	}, nil
 }
