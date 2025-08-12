ef872f1335348d8553a0b567a553948b451f0432
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-08-12 19:35:42 +0800

    新增用户查询功能

--- user/rpc/internal/logic/finduserlogic.go
+++ user/rpc/internal/logic/finduserlogic.go
@@ -2,11 +2,18 @@ package logic
 
 import (
 	"context"
-
+	"errors"
+	"fmt"
+	"strings"
+	"user/dao/models"
 	"user/rpc/internal/svc"
 	"user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+)
+
+var (
+	ErrSQL = errors.New("SQL语句查询错误")
 )
 
 type FindUserLogic struct {
@@ -24,7 +31,35 @@ func NewFindUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *FindUser
 }
 
 func (l *FindUserLogic) FindUser(in *user.FindUserReq) (*user.FindUserResp, error) {
-	// todo: add your logic here and delete this line
+	var results *[]*user.UserEntity
 
-	return &user.FindUserResp{}, nil
+	// 构造查询子句
+	condition := make([]string, 0)
+	if in.Phone != "" {
+		condition = append(condition, fmt.Sprintf("u.`phone` = '%s'", in.Phone))
+	}
+	if in.Nickname != "" {
+		condition = append(condition, fmt.Sprintf("u.`nickname` = '%s'", in.Nickname))
+	}
+	if len(in.Ids) > 0 {
+		ids := make([]string, len(in.Ids))
+		for i, id := range in.Ids {
+			ids[i] = fmt.Sprintf("'%s'", id)
+		}
+		condition = append(condition, fmt.Sprintf("u.`id` IN (%s)", strings.Join(ids, ",")))
+	}
+
+	userTable := models.User{}.TableName()
+	if err := l.svcCtx.DB.
+		Table(fmt.Sprintf("%s AS u", userTable)).
+		WithContext(l.ctx).
+		Select(fmt.Sprintf("u.`id`, u.`nickname`, u.`phone`, u.`status`, u.`sex`")).
+		Where(strings.Join(condition, " AND ")).
+		Scan(&results).Error; err != nil {
+		return nil, ErrSQL
+	}
+
+	return &user.FindUserResp{
+		User: *results,
+	}, nil
 }
