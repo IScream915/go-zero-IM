c4e502b1af0a02c1921be6246a0ea870aa67921b
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-08-13 15:56:50 +0800

    新增从context中获取用户信息功能

--- user/pkg/ctxData/data.go
+++ user/pkg/ctxData/data.go
@@ -1,1 +1,14 @@
 package ctxData
+
+import (
+	"context"
+	"errors"
+)
+
+func GetUid(ctx context.Context) (string, error) {
+	if uid, ok := ctx.Value(Identify).(string); ok {
+		return uid, nil
+	} else {
+		return "", errors.New("无法从ctx中获取用户的uid信息")
+	}
+}
