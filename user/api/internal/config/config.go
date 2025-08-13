ef54fe5085beb9016bd538f30893d3779e895af4
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-08-13 13:49:43 +0800

    更新user.api配置信息

--- user/api/internal/config/config.go
+++ user/api/internal/config/config.go
@@ -1,11 +1,14 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	rest.RestConf
-	Jwt struct {
+	UserRpc zrpc.RpcClientConf
+	Jwt     struct {
 		AccessSecret string
-		AccessExpire int64
 	}
 }
