20a262ccae5f1c71211d2d652f52681ad5da8695
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-08-13 22:54:25 +0800

    新增好友申请接口

--- social/rpc/internal/logic/friendputinlogic.go
+++ social/rpc/internal/logic/friendputinlogic.go
@@ -2,11 +2,17 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"go-zero-IM/social/dao/models"
+	"go-zero-IM/social/dao/query/implement"
+	"time"
 
 	"go-zero-IM/social/rpc/internal/svc"
 	"go-zero-IM/social/rpc/social"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"gorm.io/gorm"
 )
 
 type FriendPutInLogic struct {
@@ -23,9 +29,63 @@ func NewFriendPutInLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Frien
 	}
 }
 
-// 好友业务：请求好友、通过或拒绝申请、好友列表
+// FriendPutIn 好友业务：请求好友
 func (l *FriendPutInLogic) FriendPutIn(in *social.FriendPutInReq) (*social.FriendPutInResp, error) {
-	// todo: add your logic here and delete this line
+	// 判断与目标是否已经是好友
+	friendsTable := models.Friends{}.TableName()
+	record := &models.Friends{}
+
+	err := l.svcCtx.DB.
+		WithContext(l.ctx).
+		Table(fmt.Sprintf("%s AS f", friendsTable)).
+		Where(fmt.Sprintf("f.`user_id` = '%s' AND f.`friend_uid` = '%s'", in.UserId, in.ReqUid)).
+		First(record).Error
+
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			// 当前与目标已经是好友，无需再添加好友
+			return nil, errors.New("无法向已添加的好友发送好友申请")
+		}
+	}
+
+	// 是否已经有过未通过的申请记录
+	friendReqTable := models.FriendRequests{}.TableName()
+	result := &models.FriendRequests{}
+	// select *
+	// 	from friendReqTable AS fr
+	// 	where fr.`user_id` = x
+	// 	and fr.`req_id` = y
+	err = l.svcCtx.DB.
+		WithContext(l.ctx).
+		Table(fmt.Sprintf("%s AS fr", friendReqTable)).
+		Where(fmt.Sprintf("fr.`user_id` = '%s' AND fr.`req_uid` = '%s'", in.UserId, in.ReqUid)).
+		First(result).Error
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errors.New("查询" + friendReqTable + "时出现了问题" + err.Error())
+		}
+	} else {
+		// 如果查询结果不为空并且处理结果为通过的时候返回错误
+		if result.HandleResult == models.ResultApprove {
+			return nil, errors.New("无法向已添加的好友发送好友申请")
+		}
+	}
+
+	// 创建申请记录
+	if txErr := l.svcCtx.DB.Transaction(func(tx *gorm.DB) error {
+		txDbTool := implement.NewDbToolHelper[models.FriendRequests](tx)
+		if err = txDbTool.InsertSingleRecord(l.ctx, &models.FriendRequests{
+			UserID:  in.UserId,
+			ReqUID:  in.ReqUid,
+			ReqMsg:  in.ReqMsg,
+			ReqTime: time.Now(),
+		}); err != nil {
+			return err
+		}
+		return nil
+	}); txErr != nil {
+		return nil, errors.New("创建好友申请时出现了问题" + txErr.Error())
+	}
 
 	return &social.FriendPutInResp{}, nil
 }
