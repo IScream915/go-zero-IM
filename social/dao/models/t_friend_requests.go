203c2d5cef957ce6db24ee3b712bd2aad35cdf15
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-08-13 22:54:55 +0800

    修复字段数据格式

--- social/dao/models/t_friend_requests.go
+++ social/dao/models/t_friend_requests.go
@@ -6,15 +6,21 @@ import (
 	"gorm.io/gorm"
 )
 
+const (
+	ResultUnhandled = iota
+	ResultRefuse
+	ResultApprove
+)
+
 type FriendRequests struct {
 	ID           uint           `gorm:"column:id; type:int(11) unsigned; primarykey; autoIncrement; comment:主键ID" json:"id"`
 	UserID       string         `gorm:"column:user_id; type:varchar(64); not null; comment:用户ID" json:"user_id"`
 	ReqUID       string         `gorm:"column:req_uid; type:varchar(64); not null; comment:请求用户ID" json:"req_uid"`
 	ReqMsg       string         `gorm:"column:req_msg; type:varchar(255); default:'' ;comment:请求消息" json:"req_msg"`
 	ReqTime      time.Time      `gorm:"column:req_time; type:timestamp; not null; comment:请求时间" json:"req_time"`
-	HandleResult int            `gorm:"column:handle_result; type:tinyint; default:0; comment:处理结果 0-拒绝, 1-通过" json:"handle_result"`
+	HandleResult int            `gorm:"column:handle_result; type:tinyint; default:0; comment:处理结果 0-未处理, 1-拒绝, 2-通过" json:"handle_result"`
 	HandleMsg    string         `gorm:"column:handle_msg; type:varchar(255); default:''; comment:处理消息" json:"handle_msg"`
-	HandledAt    time.Time      `gorm:"column:handled_at; type:timestamp; comment:处理时间" json:"handled_at"`
+	HandledAt    *time.Time     `gorm:"column:handled_at; type:timestamp; comment:处理时间" json:"handled_at"`
 	DeletedAt    gorm.DeletedAt `gorm:"column:deleted_at; type:timestamp; comment:删除时间" json:"deleted_at"`
 }
 
--- social/dao/models/t_group_requests.go
+++ social/dao/models/t_group_requests.go
@@ -15,8 +15,8 @@ type GroupRequests struct {
 	JoinSource    int            `gorm:"column:join_source; type:tinyint; default:0; comment:加入来源 0-搜索群聊, 1-推荐进群, 2-创建群聊进群" json:"join_source"`
 	InviterUserID string         `gorm:"column:inviter_user_id; type:varchar(64); comment:邀请者用户ID" json:"inviter_user_id"`
 	HandleUserID  string         `gorm:"column:handle_user_id; type:varchar(64); comment:处理者用户ID" json:"handle_user_id"`
-	HandleTime    time.Time      `gorm:"column:handle_time; type:timestamp; comment:处理时间" json:"handle_time"`
-	HandleResult  int            `gorm:"column:handle_result; type:tinyint; default:0; comment:处理结果 0-拒绝, 1-成功" json:"handle_result"`
+	HandleTime    *time.Time     `gorm:"column:handle_time; type:timestamp; comment:处理时间" json:"handle_time"`
+	HandleResult  int            `gorm:"column:handle_result; type:tinyint; default:0; comment:处理结果 0-未处理, 1-拒绝, 2-成功" json:"handle_result"`
 	DeletedAt     gorm.DeletedAt `gorm:"column:deleted_at; type:timestamp; comment:删除时间" json:"deleted_at"`
 }
 
