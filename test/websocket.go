171083b2f7724411d58019ff330fccdba90cd5f2
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-08-14 20:52:19 +0800

    新增websocket测试包

---
+++ test/websocket.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/echo", echo)
+	fmt.Println("websocket已启动, 正在监听...")
+	log.Fatal(http.ListenAndServe("127.0.0.1:1111", nil))
+}
