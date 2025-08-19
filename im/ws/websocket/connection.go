3dd07ed58079bae738c4057f0f93867a1b9dbe48
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-08-19 20:23:06 +0800

    新增连接服务

---
+++ im/ws/websocket/connection.go
@@ -0,0 +1,109 @@
+package websocket
+
+import (
+	"math"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	defaultMaxConnectionIdle = time.Duration(math.MaxInt64)
+	defaultAckTimeout        = 30 * time.Second
+)
+
+type Conn struct {
+	idleMu sync.Mutex
+
+	*websocket.Conn
+	s *Server
+
+	idle              time.Time
+	maxConnectionIdle time.Duration
+
+	done chan struct{}
+}
+
+func NewConn(s *Server, w http.ResponseWriter, r *http.Request) *Conn {
+	c, err := s.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		s.Errorf("upgrade err %v", err)
+		return nil
+	}
+
+	conn := &Conn{
+		Conn:              c,
+		s:                 s,
+		idle:              time.Now(),
+		maxConnectionIdle: s.opt.maxConnectionIdle,
+		//readMessage:       make([]*Message, 0, 2),
+		//readMessageSeq:    make(map[string]*Message, 2),
+		//message:           make(chan *Message, 1),
+		done: make(chan struct{}),
+	}
+
+	go conn.keepalive()
+	return conn
+}
+
+func (c *Conn) ReadMessage() (messageType int, p []byte, err error) {
+	messageType, p, err = c.Conn.ReadMessage()
+
+	c.idleMu.Lock()
+	defer c.idleMu.Unlock()
+	c.idle = time.Time{}
+	return
+}
+
+func (c *Conn) WriteMessage(messageType int, data []byte) error {
+	c.idleMu.Lock()
+	defer c.idleMu.Unlock()
+
+	// 方法是并不安全的
+	err := c.Conn.WriteMessage(messageType, data)
+	c.idle = time.Now()
+	return err
+}
+
+func (c *Conn) Close() error {
+	select {
+	case <-c.done:
+	default:
+		close(c.done)
+	}
+
+	return c.Conn.Close()
+}
+
+func (c *Conn) keepalive() {
+	idleTimer := time.NewTimer(c.maxConnectionIdle)
+	defer func() {
+		idleTimer.Stop()
+	}()
+
+	for {
+		select {
+		case <-idleTimer.C:
+			c.idleMu.Lock()
+			idle := c.idle
+			if idle.IsZero() { // The connection is non-idle.
+				c.idleMu.Unlock()
+				idleTimer.Reset(c.maxConnectionIdle)
+				continue
+			}
+			val := c.maxConnectionIdle - time.Since(idle)
+			c.idleMu.Unlock()
+			if val <= 0 {
+				// The connection has been idle for a duration of keepalive.MaxConnectionIdle or more.
+				// Gracefully close the connection.
+				c.s.Close(c)
+				return
+			}
+			idleTimer.Reset(val)
+		case <-c.done:
+			return
+		}
+	}
+}
